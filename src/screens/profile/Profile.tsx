6dc215ec9f714853be4b3b1caf5ce63a3457021e
Mateusz Szymański <mateusz.m.szymanski@gmail.com>
2025-02-10 04:33:47 +0100

    readme update

--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { colors, dimensions } from '../../theme';
 import { useUser } from '../../store/user/useUser';
 import Txt from '../../components/Txt';
